a73c4336d8ed22cc80804080560623b4451d8d9e
Flozzie443 <owinoflorence443@gmail.com>
2025-09-29 09:38:34 -0700

    Add Counter component with useState and integrate into App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+import Counter from './components/Counter'; // ✅ Import Counter
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <MainContent />
       <UserProfile 
@@ -13,10 +14,10 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+      <Counter />   {/* ✅ Use Counter here */}
       <Footer />
-    </>
+    </div>
   );
 }
 
 export default App;
-
